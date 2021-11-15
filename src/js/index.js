5817c8a74f4cc1423942d8db3a93b03e3c229214
vipin singh <svipin174@gmail.com>
2021-11-15 14:24:51 +0530

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// =====================================================================
+// 
+// =====================================================================
+import Swiper from 'swiper/bundle';
+
+var swiper = new Swiper(".ngoBanner", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
